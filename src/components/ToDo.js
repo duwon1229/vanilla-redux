fc9e6cc987178f2d93ebfd70cecf48c98d840356
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2021-09-06 19:41:17 +0900

    Add: reduxjs toolkit

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-import { acrionCreators, actionCreators } from "../store";
+import { remove } from "../store";
 import { Link } from "react-router-dom";
 
 function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DEl</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>{" "}
+      <button onClick={onBtnClick}>DEl</button>
     </li>
   );
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
-    onBtnClick: () => dispatch(acrionCreators.deleteToDo(ownProps.id)),
+    onBtnClick: () => dispatch(remove(ownProps.id)),
   };
 }
 
